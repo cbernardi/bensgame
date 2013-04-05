f0de7080a32ecd9a058ba09f2574ddab850ccf72
Chris Bernardi <chris@3sunset.com>
2013-04-05 00:10:41 -0400

    First commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Ben's Game
+========
+
+Simple choose your own adventure game written in Python.
---
+++ bens_game.py
@@ -0,0 +1,110 @@
+print "Welcome to the Ben's Game. You choose the path for this story. What's your name?"
+
+name = raw_input("> ")
+
+print "Welcome %s. Shall we begin? Y or N" % name
+
+answer = raw_input("> ")
+
+if answer == "Y":
+	print "You're in a neighborhood with four houses. Which house do you choose? 1, 2, 3, 4"
+
+	house = raw_input("> ")
+
+	# house 1
+	if house == "1":
+		print "The first house is an old mansion. What do you do?"
+		print "1. Go inside."
+		print "2. Pick another house."
+
+		house1_choice1 = raw_input("> ")
+
+		if house1_choice1 == "1":
+			print "Inside is dark and there are two hallways. Which one do you go down?"
+			print "1. Go left."
+			print "2. Go right."
+
+			house1_choice2 = raw_input("> ")
+
+			if house1_choice2 == "1":
+				print "You get sucked into a time portal and find yourself surrounded by lava. You disintegrate. The end."
+			else:
+				print "At the end of the hallway is a great room with a large table covered in all your favorite foods. Well done!"
+
+		else:
+			print "Really? You are changing your mind?! I quit. The end."
+
+	# house 2
+	elif house == "2":
+		print "The second house is a small cottage. What do you do?"
+		print "1. Go inside."
+		print "2. Pick another house."
+
+		house2_choice1 = raw_input("> ")
+
+		if house2_choice1 == "1":
+			print "Inside its dimly lit and cold and there are two doors. Which one do you choose?"
+			print "1. Left door."
+			print "2. Right door."
+
+			house2_choice2 = raw_input("> ")
+
+			if house2_choice2 == "1":
+				print "You open the left door to find a room filled with treaures. Well done!"
+			else:
+				print "Behind the right door is more darkness. You step forward and fall in a bottomless pit. The end."
+
+		else:
+			print "Really? You are changing your mind?! I quit. The end."
+
+	# house 3
+	elif house == "3":
+		print "The third house is a brick house. What do you do?"
+		print "1. Go inside."
+		print "2. Pick another house."
+
+		house3_choice1 = raw_input("> ")
+
+		if house3_choice1 == "1":
+			print "Its hot inside and smells like dirt. You see two staircases going up. Which do you choose?"
+			print "1. Go left."
+			print "2. Go right."
+
+			house3_choice2 = raw_input("> ")
+
+			if house3_choice2 == "1":
+				print "As you walk up the left stairs, they begin to shake. Next thing you know, they collapse and you slide in to a pit opf snakes. The end."
+			else:
+				print "As you walk up the right staircase, the air gets cooler and smells of saltwater. You find yourself sailing away on a boat to your own private island."
+
+		else:
+			print "Really? You are changing your mind?! I quit. The end."
+
+	# house 4
+	else:
+		print "The second house is a small cottage. What do you do?"
+		print "1. Go inside."
+		print "2. Pick another house."
+
+		house2_choice1 = raw_input("> ")
+
+		if house2_choice1 == "1":
+			print "Inside is dark and there are two hallways. Which one do you go down?"
+			print "1. Go left."
+			print "2. Go right."
+
+			house2_choice2 = raw_input("> ")
+
+			if house2_choice2 == "1":
+				print "You get sucked into a time portal and find yourself surrounded by lava. You disintegrate."
+			else:
+				print "Goog ending"
+
+		else:
+			print "Really? You are changing your mind?! I quit. The end."
+
+else:
+	print "Sorry to hear that %s. Maybe another time." % name
+
+
+
